ce93197dc5140d32376712b70a081a8e3e0444ab
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-05-17 13:01:56 -0300

    feat: backend rota para criar doação

--- backend/src/donations/donations.service.ts
+++ backend/src/donations/donations.service.ts
@@ -1,13 +1,15 @@
 import { Prisma } from '.prisma/client';
 import { Injectable } from '@nestjs/common';
-import { CreateDonationInput } from './dto/create-donation.input';
 import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class DonationsService {
   constructor(private prisma: PrismaService) {}
-  create(createDonationInput: CreateDonationInput) {
-    return 'This action adds a new donation';
+
+  async create(createDonationInput: Prisma.DonationCreateInput) {
+    return await this.prisma.donation.create({
+      data: createDonationInput,
+    });
   }
 
   async findAll() {
