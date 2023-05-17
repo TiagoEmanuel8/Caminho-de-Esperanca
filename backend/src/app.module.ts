1a528f81a32e551f05b1a2f9ad743cf2776c0262
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-05-17 16:46:56 -0300

    feat: implementando subscription total doações

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,22 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-// import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { DonationsModule } from './donations/donations.module';
 import { GraphQLDateTime } from 'graphql-scalars';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
-  // 1
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: false,
-      // plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
       typePaths: ['./**/*.graphql'],
       resolvers: { DateTime: GraphQLDateTime },
+      subscriptions: {
+        'graphql-ws': true,
+        'subscriptions-transport-ws': true,
+      },
     }),
     DonationsModule,
   ],
