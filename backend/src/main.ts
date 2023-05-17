44257c0282fba1c56ebce5877c0a1fc7ba3421aa
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-05-17 17:01:47 -0300

    feat: implementando cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,PATCH,POST,DELETE',
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
