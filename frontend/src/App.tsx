c226e7565dd1f00e85cf8b9787b55a720bc563f4
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-05-17 21:29:25 -0300

    feat: adicionando logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,37 @@
+import '@fontsource/montserrat/700.css';
+import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/300.css';
+
 import * as React from "react"
 import {
   ChakraProvider,
   Box,
   Text,
   VStack,
-  Code,
   Grid,
-  theme,
+  Heading,
+  extendTheme
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./Logo"
 
+const theme = extendTheme({
+  fonts: {
+    heading: 'Montserrat',
+    body: 'Montserrat',
+  },
+});
+
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
+      <Grid minH="100vh" p={3} bg="gray.50">
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
+          <Logo h="32vmin" pointerEvents="none" />
+          <Heading as="h1" size="xl">Ajude você também</Heading>
           <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+            Muitas pessoas estão necessitadas nesse momento, dê uma esperança a elas.
           </Text>
         </VStack>
       </Grid>
