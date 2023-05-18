89f8bcce56bbba775a4090d48093d6671898057b
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-05-17 21:20:10 -0300

    feat: adicionando logo

---
+++ frontend/src/Logo.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+import {
+  chakra,
+  keyframes,
+  ImageProps,
+  forwardRef,
+} from "@chakra-ui/react"
+import logo from "./logo.png"
+
+const spin = keyframes`
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+`
+
+export const Logo = forwardRef<ImageProps, "img">((props, ref) => {
+  return <chakra.img src={logo} ref={ref} {...props} />
+})
